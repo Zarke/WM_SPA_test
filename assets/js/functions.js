f38eefee865beee7da159790b884d7b0f6771fc6
Zarke <milanzaric54@gmail.com>
2018-06-26 15:28:37 +0200

    added checked icon on My Feed and My Favourites

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,4 +4,11 @@ function getData(){
         questions = json;
     })
 }
-
+//checks if value is true and sets checked icon accordingly
+function isTrue(table,colIndx){
+    table.rows().every(function (rowIdx) {
+    if(table.cell(rowIdx, colIndx).data()){
+        table.cell(rowIdx, colIndx).data('<i class="fas fa-check"></i>')
+    }
+  }).draw();
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@ getData()
 
 $(function(){
     $("#entry").view(questions);
-    var table = $("table").DataTable();
-    
+    var table = $("table").DataTable(
+        {"dom": '<"top"f>rt<"bottom"lp><"clear">'}
+    );
+    //sets my Feed and my Favourites checked icons
+    isTrue(table,3);
+    isTrue(table,4);
 })
