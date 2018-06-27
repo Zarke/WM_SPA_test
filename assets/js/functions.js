327af33c535f6862c8fc53769efa95f92189065e
Zarke <milanzaric54@gmail.com>
2018-06-27 15:40:40 +0200

    added altering row data

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -14,3 +14,48 @@ function isTrue(table,colIndx){
     }
   }).draw();
 }
+
+//displays the uploaded image after short progress bar load
+function getImage(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('.progress').removeClass('d-none')
+            $('#upload i').addClass('d-none')
+            $('.progress-bar').animate({width:'100%'},700,function(){
+                $('.progress').addClass('d-none')
+                $('.progress-bar').css('width', '0%').attr('aria-valuenow', 0); 
+                $('#upload').css('background-image','url(' + e.target.result + ')');  
+            })
+            
+            
+        };  
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+//returns the new values for the invoked row
+function alterRow(data){
+    if(!$('#tagName').val()){
+        $('#tagName').popover("show")
+        setTimeout(function(){
+            $('#tagName').popover("hide")  
+        },700)
+    } else {
+        data[1] = $('#tagName').val()//tagName
+        data[2] = $('#tagTypes').val();//chosen tagType
+        if(!$('#myFeed').is(':checked')){
+            //if myFeedis ont checked this cell will be empty on render
+        } else {data[3] = '<i class="fas fa-check"></i>'; console.log('cekiran je')}//if it is checked it will have the value of the checked icon 
+        if(!$('#myFav').is(':checked')){
+            data[4] = ''
+        } else{data[4] = '<i class="fas fa-check"></i>'}
+    }
+    return data
+}
+
+//resets image upload
+function removeImg(){
+    $('#upload').css('background-image', 'url()');
+    $('#upload i').removeClass('d-none')
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -17,10 +17,29 @@ $(function(){
         table.row( $(this).parents('tr') ).remove().draw();
     } );
 
-    $('table tbody').on( 'click', 'tr', function () {
-        console.log( table.row( this ).data() );
+    //fill out the modal with the information from the clicked row
+    $('table tbody').on('click', 'tr', function () {
+        var data = table.row( this ).data();
+        var self = this;
+        console.log()
+        $('#tagName').val(data[1])
+        if(!data[3]){
+            $('#myFeed').prop( "checked", false );
+        }
+        if(!data[4]){
+            $('#myFav').prop( "checked", false );
+        }
+        if(!data[2]){
+            $('#tagTypes').val(0);
+        } else { 
+            $('#tagTypes').val(data[2]) 
+        }
+        $('#save').on('click',$.proxy(function(){
+            data = alterRow(data)
+            table.row(this).data(data);
+        }, this ))
+       
     } );
-
-    
-    
 })
+
+
