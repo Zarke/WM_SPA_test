f9bab2b12f19d0bcc9fabd9b2244d9dd3b8872ec
Zarke <milanzaric54@gmail.com>
2018-06-28 14:44:53 +0200

    loading data with an ajax call

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,16 +1,9 @@
-//get data form the JSON file
-function getData(item){
-    $.getJSON('assets/data.json', function(json) {
-        questions = json;
-    })
-    localStorage.setItem("tagTypes", JSON.stringify(item));
-    
-}
+
 //checks if value is true and sets checked icon accordingly
-function isTrue(table,colIndx){
+function setIcons(table,colIndx){
     table.rows().every(function (rowIdx) {
     if(table.cell(rowIdx, colIndx).data()){
-        table.cell(rowIdx, colIndx).data('<i class="fas fa-check"></i>')
+        table.cell(rowIdx, colIndx).data('<i class="fas fa-check"></i>');
     }
   }).draw();
 }
@@ -20,10 +13,10 @@ function getImage(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            $('.progress').removeClass('d-none')
-            $('#upload i').addClass('d-none')
+            $('.progress').removeClass('d-none');
+            $('#upload i').addClass('d-none');
             $('.progress-bar').animate({width:'100%'},700,function(){
-                $('.progress').addClass('d-none')
+                $('.progress').addClass('d-none');
                 $('.progress-bar').css('width', '0%').attr('aria-valuenow', 0); 
                 $('#upload').css('background-image','url(' + e.target.result + ')');  
             })
@@ -37,25 +30,38 @@ function getImage(input) {
 //returns the new values for the invoked row
 function alterRow(data){
     if(!$('#tagName').val()){
-        $('#tagName').popover("show")
+        $('#tagName').popover("show");
         setTimeout(function(){
-            $('#tagName').popover("hide")  
+            $('#tagName').popover("hide");  
         },700)
     } else {
-        data[1] = $('#tagName').val()//tagName
-        data[2] = $('#tagTypes').val();//chosen tagType
-        if(!$('#myFeed').is(':checked')){
-            //if myFeedis ont checked this cell will be empty on render
-        } else {data[3] = '<i class="fas fa-check"></i>'}//if it is checked it will have the value of the checked icon 
+        data.tagName = $('#tagName').val();//tagName
+        data.tagType = $('#tagTypes').val();//chosen tagType
+        if(!$('#myFeed').is(':checked')){ //if myFeed is not checked this cell will be empty on render
+            data.myFeed = '';
+        } else {data.myFeed = '<i class="fas fa-check"></i>';}//if it is checked it will have the value of the checked icon 
         if(!$('#myFav').is(':checked')){
-            data[4] = ''
-        } else{data[4] = '<i class="fas fa-check"></i>'}
+            data.myFavourites = '';
+        } else{data.myFavourites = '<i class="fas fa-check"></i>';}
     }
-    return data
+    return data;
 }
 
 //resets image upload
 function removeImg(){
     $('#upload').css('background-image', 'url()');
-    $('#upload i').removeClass('d-none')
+    $('#upload i').removeClass('d-none');
+}
+
+//creates the TagId so taht the new entry will be in the last place in the default pagination
+function TagId(table){
+    var maxTagId = table.cell(0,0).data();
+    $(table.rows().nodes()).each(function(rowIdx){
+        if(table.cell(rowIdx, 0).data() > maxTagId){
+            maxTagID = table.cell(rowIdx,0).data();
+            console.log(maxTagId);
+        }
+    })
+
+    return maxTagId;
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,87 @@
-var questions;
-var tagTypes = ["Person","City","Car","Football Club","Object"]
-getData(tagTypes)
+var entries;//varaible that stores table entries
+var tagTypes = ["Person","City","Car","Football Club","Object"];
+var rowData = ["","","","","","<i class='fas fa-pen' data-toggle='modal' data-target='#inputModal'></i> <i class='fas fa-times'></i>"];//variable that holds data for the inserted row
 
 $(function(){
-    $("#entry").view(questions);
+    //loads tagTypes into the dropdown in the modal
     $('#tagTypes').view(JSON.parse(localStorage.getItem("tagTypes")))
-    var table = $("table").DataTable(
-        {"dom": '<"top"f>rt<"bottom"lp><"clear">'}
+
+    //loads data via ajax call and defines a statis actions column taht is the same for all entries
+    var table = $("table").DataTable({
+        'dom': '<"top"f>rt<"bottom"lp><"clear">',
+        'ajax'       : {
+            "type"   : "POST",
+            "url"    : "assets/data.json",
+            "dataSrc": function (json) {
+              var return_data = new Array();
+              for(var i=0;i< json.length; i++){
+                return_data.push({
+                  'tagID': json[i].tagID,
+                  'tagName'  : json[i].tagName,
+                  'tagType' : json[i].tagType,
+                  'myFeed' : json[i].myFeed,
+                  'myFavourites' : json[i].myFavourites,
+                  'actions' : '<i class="fas fa-pen" data-toggle="modal" data-target="#inputModal"></i> <i class="fas fa-times"></i>'
+                })
+              }
+              return return_data;
+            }
+          },
+          "columns"    : [
+            {'data': 'tagID'},
+            {'data': 'tagName'},
+            {'data': 'tagType'},
+            {'data': 'myFeed'},
+            {'data': 'myFavourites'},
+            {'data': 'actions'},
+          ],
+          "fnInitComplete": function(oSettings, json) {
+            setIcons(table,3);
+            setIcons(table,4);
+          }
+      
+    }
     );
-    //sets my Feed and my Favourites checked icons
-    isTrue(table,3);
-    isTrue(table,4);
 
     //deletes the row when the delete icon is clicked
     $('table tbody').on( 'click', 'i.fa-times', function () {
         table.row( $(this).parents('tr') ).remove().draw();
     } );
 
+    // $('table tbody').on('click', 'tr', function () {
+    //     console.log(table.row(this).data());
+    // });
     //fill out the modal with the information from the clicked row
     $('table tbody').on('click', 'tr', function () {
-        var data = table.row(this).data();
-        $('#tagName').val(data[1])
-        if(!data[3]){
+        data = table.row(this).data();
+        $('#tagName').val(data.tagName);
+        if(!data.myFeed){
             $('#myFeed').prop( "checked", false );
-        }
-        if(!data[4]){
+        } else{$('#myFeed').prop( "checked", true );}
+        if(!data.myFavourites){
             $('#myFav').prop( "checked", false );
-        }
-        if(!data[2]){
+        } else {$('#myFav').prop( "checked", true );}
+        if(!data.tagType){
             $('#tagTypes').val(0);
         } else { 
-            $('#tagTypes').val(data[2]) 
+            $('#tagTypes').val(data.tagType) ;
         }
         $('#save').one('click',$.proxy(function(){
-            data = alterRow(data)
+            data = alterRow(data);
             table.row(this).data(data);
         }, this ))
-       
     } );
+
+    //adds a new row and goes to the last page of the pagination
+    // $('#save').on('click',function(){
+    //     rowData = alterRow(rowData)
+    //     rowData[0] = TagId(table) + 1; 
+    //     console.log(rowData[0])
+    //     console.log(table.rows().nodes())
+    //     table.row.add(rowData).draw()
+    //     table.page('last').draw()
+    //     $('#inputModal').modal('hide');
+    // })
 })
 
 
